992caff7c7c8e508231bc9a25f92dfbd9e417af4
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2025-12-31 10:12:30 +0100

    there was an issue with finding welcomeActivity but i resolved the conflict

--- app/src/main/java/com/example/chatapplication/ui/LaunchActivity.kt
+++ app/src/main/java/com/example/chatapplication/ui/LaunchActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.example.chatapplication.WelcomeActivity
 import com.example.chatapplication.databinding.ActivityLaunchBinding
 import com.google.firebase.auth.FirebaseAuth
 
