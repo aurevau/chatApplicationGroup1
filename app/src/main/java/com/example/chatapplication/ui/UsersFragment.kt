2b68adc8d5a6fe11a4d7a5bd675b23cb5209221b
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-01 18:50:04 +0100

    Adapter and viewmodel was implemented inside AllUsersFragment

--- app/src/main/java/com/example/chatapplication/ui/UsersFragment.kt
+++ app/src/main/java/com/example/chatapplication/ui/UsersFragment.kt
@@ -5,53 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatapplication.R
+import com.example.chatapplication.adapter.UserRecyclerAdapter
+import com.example.chatapplication.databinding.FragmentUsersBinding
+import com.example.chatapplication.viewmodel.UserViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UsersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UsersFragment : Fragment() {
+
+    private lateinit var binding: FragmentUsersBinding
+
+    private lateinit  var adapter: UserRecyclerAdapter
+
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        viewModel = ViewModelProvider(requireActivity())[UserViewModel::class.java]
+        adapter = UserRecyclerAdapter({user ->
+            // Start New chatroom from user or open existing chatroom. Need ChatRoomRepository for this!
+        })
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false)
+        binding = FragmentUsersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UsersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UsersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvUsers.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvUsers.adapter = adapter
+
+        viewModel.user.observe(viewLifecycleOwner) { list ->
+            adapter.submitList(list)
+
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged()
+    }
+
+
 }
