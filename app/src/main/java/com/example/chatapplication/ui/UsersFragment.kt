8bf1c606f8926c335a45fdd17a507f904c75b024
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-05 23:34:13 +0100

    implemented recentSearch delete on longitemclick

--- app/src/main/java/com/example/chatapplication/ui/UsersFragment.kt
+++ app/src/main/java/com/example/chatapplication/ui/UsersFragment.kt
@@ -18,6 +18,7 @@ import com.example.chatapplication.adapter.UserRecyclerAdapter
 import com.example.chatapplication.data.User
 import com.example.chatapplication.databinding.FragmentUsersBinding
 import com.example.chatapplication.repository.MessageRepository
+import com.example.chatapplication.repository.UserRepository
 import com.example.chatapplication.viewmodel.ChatViewModel
 import com.example.chatapplication.viewmodel.UserViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -43,6 +44,7 @@ class UsersFragment : Fragment() {
     private val selectedUsersSet = mutableSetOf<User>()
 
     private lateinit var groupChatButton: Button
+    private val userRepository = UserRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +53,7 @@ class UsersFragment : Fragment() {
         chatViewModel = ViewModelProvider(requireActivity())[ChatViewModel::class.java]
 
         val currentUserId = viewModel.getCurrentUserId()
+        viewModel.loadRecentSearches()
 
         selectedUsersAdapter = SelectedUsersRecyclerAdapter({ removedUser ->
             selectedUsersSet.remove(removedUser)
@@ -70,7 +73,9 @@ class UsersFragment : Fragment() {
             // Se mer information om användaren och kunna lägga till vän?
             binding.cvSearchUser.visibility = View.GONE
             binding.etSearchUser.text?.clear()
-            viewModel.addRecentSearch(user)
+            if (currentUserId != null) {
+                viewModel.addRecentSearchToFirebase(currentUserId, user)
+            }
 
         }, { user ->
             // Start New chatroom from user or open existing chatroom. Need ChatRoomRepository for this!
@@ -101,6 +106,8 @@ class UsersFragment : Fragment() {
                 if (selectedUsersSet.size > 1) View.VISIBLE else View.GONE
             binding.rvSelectedUsers.visibility =
                 if (selectedUsersSet.size > 1) View.VISIBLE else View.GONE
+        }, {user ->
+            userRepository.deleteRecentSearch(user)
         })
 
 
@@ -130,7 +137,7 @@ class UsersFragment : Fragment() {
 
         val currentUserId = viewModel.getCurrentUserId() ?: return
 
-        viewModel.loadRecentSearches()
+//        viewModel.loadRecentSearches()
 
         groupChatButton = binding.btnStartGroupChat
 
@@ -223,6 +230,10 @@ class UsersFragment : Fragment() {
 
         adapter.notifyDataSetChanged()
     }
+
+
+
+
 
 
 }
