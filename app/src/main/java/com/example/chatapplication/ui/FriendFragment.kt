829399abeba8986873c426efa32f3305c7ae4412
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-06 12:34:40 +0100

    fixed issues with friend and addeed friendsfragmentRecycler

--- app/src/main/java/com/example/chatapplication/ui/FriendFragment.kt
+++ app/src/main/java/com/example/chatapplication/ui/FriendFragment.kt
@@ -1,16 +1,51 @@
 package com.example.chatapplication.ui
-
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatapplication.adapter.FriendRecyclerAdapter
 import com.example.chatapplication.databinding.FragmentFriendBinding
+import com.example.chatapplication.viewmodel.UserViewModel
 
 
 class FriendFragment : DialogFragment() {
 
+
+    private lateinit var viewModel: UserViewModel
+    private lateinit var recyclerView: RecyclerView
     private lateinit var binding: FragmentFriendBinding
+
+    private lateinit var adapter: FriendRecyclerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity())[UserViewModel::class.java]
+
+        val currentUserId = viewModel.getCurrentUserId()
+        if (currentUserId != null) {
+            viewModel.getFriends(currentUserId)
+        }
+
+        adapter = FriendRecyclerAdapter({user ->
+            val chatIntent = Intent(activity, ChatActivity::class.java)
+            chatIntent.putExtra("USER_ID", user.id)
+            startActivity(chatIntent)
+        }, {user ->
+            viewModel.addFriend(currentUserId, user)
+        }, {user ->
+            viewModel.removeFriend(currentUserId, user)
+        }, {user ->
+            // Implementera om man vill komma till en persons profil?
+        })
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +62,23 @@ class FriendFragment : DialogFragment() {
         return binding.root
 
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = binding.rvFriends
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+
+
+        viewModel.friends.observe(viewLifecycleOwner) {friendList ->
+            adapter.updateFriendList(friendList)
+        }
+    }
+
+
+
+
 
     override fun onStart() {
         super.onStart()
